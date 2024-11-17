871afcfea5f6ae5c0420fc725ba15d939ce2e7f6
Gauravpadam <gauravpadam2@gmail.com>
2024-11-17 20:48:50 +0530

    JWT implemented

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
-from enum import verify
 from models.user_model import User_Model
 from schemas.user_schema import Register_User, Login_User
 from database import Base, engine, _Session
 from fastapi import FastAPI, Depends, HTTPException, status
+from auth_bearer import jwt_bearer
 from utils import *
 import uvicorn
 from sqlalchemy.orm import Session
@@ -45,25 +45,29 @@ def sign_up(user: Register_User, session: Session = Depends(get_session)):
             "User already exists"
         )
 
-    user_password = get_hashed_password(user.password)
+    hashed_password = get_hashed_password(user.password)
 
     insert_query = """
     INSERT INTO USERS (username, email, password)
     VALUES (:username, :email, :password)
     """
+
     session.execute(text(insert_query), {
         "username": user.username,
         "email": user.email,
-        "password": user.password
+        "password": hashed_password
     })
 
     session.commit()
 
     return {"message": "User registration successful!"}
 
-@app.get("/login")
+@app.post("/login")
 def login(user: Login_User, session: Session = Depends(get_session)):
-    existing_user = session.execute(text("SELECT 1 FROM USERS WHERE email= :email"), {"email": user.email}).fetchone()
+    print(user)
+    existing_user = session.execute(text("SELECT * FROM USERS WHERE email= :email"), {"email": user.email}).fetchone()
+
+    print(existing_user)
 
     if not existing_user:
         raise HTTPException(404, "User not found")
@@ -75,8 +79,37 @@ def login(user: Login_User, session: Session = Depends(get_session)):
     if not pass_verify:
         raise HTTPException(400, "Passwords do not match!")
 
+    existing_user_id = existing_user.id
 
+    access_token = create_access_token(existing_user_id)
+    refresh_token = create_refresh_token(existing_user_id)
 
+    insert_query = """
+    INSERT INTO TOKENS (user_id, access_token, refresh_token, status)
+    VALUES (:user_id, :access_token, :refresh_token, :status)
+    """
+
+    session.execute(text(insert_query),{
+        "user_id": existing_user_id,
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "status": True
+    })
+
+    session.commit()
+
+    return {"message": "Login successful!", "access_token": access_token, "refresh_token": refresh_token}
+
+# Accessing the jwt via depends over here
+@app.get("/get_users")
+def get_users(token=Depends(jwt_bearer), session: Session = Depends(get_session)):
+    query = "SELECT * FROM USERS"
+    try:
+        users = session.execute(text(query)).all()
+        # Convert to a list of dictionaries
+        return [user._mapping for user in users]
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 # Main entry point for Uvicorn
--- backend/utils.py
+++ backend/utils.py
@@ -2,8 +2,8 @@ import os
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from conf import ALGORITHM, JWT_SECRET_KEY, JWT_REFRESH_SECRET_KEY, ACCESS_TOKEN_EXPIRE, REFRESH_TOKEN_EXPIRE
-from typing import Union, Any
 from jose import jwt
+from typing import Union, Any
 
 assert (ACCESS_TOKEN_EXPIRE, REFRESH_TOKEN_EXPIRE) != (None, None)
 
@@ -16,6 +16,17 @@ def verify_password(password: str, hashed_pass: str) -> bool:
     return password_context.verify(password, hashed_pass)
 
 def create_access_token(subject: Union[str, Any], expires_delta: Union[int, Any] = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + timedelta(seconds = expires_delta)
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes = (float(ACCESS_TOKEN_EXPIRE) if ACCESS_TOKEN_EXPIRE else 30))
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
+
+def create_refresh_token(subject: Union[str, Any], expires_delta: Union[int, Any] = None) -> str:
     if expires_delta is not None:
         expires_delta = datetime.utcnow() + timedelta(seconds = expires_delta)
     else:
