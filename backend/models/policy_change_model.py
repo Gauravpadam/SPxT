0984cb78134b7f0d859bd487af518c839c524977
ykoyande <yash.koyande@here.com>
2024-11-20 00:07:12 +0530

    added policy_change model

---
+++ backend/models/policy_change_model.py
@@ -0,0 +1,13 @@
+from asyncio import Handle
+from sqlalchemy import Column, String, Integer, DateTime, Boolean
+from database.database import Base
+import datetime
+
+class Policy_Change_Model(Base):
+    __tablename__ = "policy_change"
+    policy_change_id = Column(Integer, primary_key=True)
+    chapter_number = Column(Integer)
+    itc_hs = Column(Integer)
+    description = Column(String(200))
+    policy_condition = Column(String(200))
+    updated_at = Column(DateTime, default=datetime.datetime.now)
