cfd33b51765ed3cc0a3f73d8d686751a5fc5f76f
Gauravpadam <gauravpadam2@gmail.com>
2024-11-20 16:43:43 +0530

    Added chatbot route

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
+from routes import healthcheck, user, alerts, products, chatbot
 from database.database import Base, engine
-from routes import healthcheck, user, alerts, products
 
 Base.metadata.create_all(engine)
 
@@ -19,6 +18,7 @@ app.include_router(healthcheck.router)
 app.include_router(user.router)
 app.include_router(alerts.router)
 app.include_router(products.router)
+app.include_router(chatbot.router)
 
 if __name__ == "__main__":
     import uvicorn
