fb875c81d3affd163a1c97c2cbf694f1431e33a5
ykoyande <yash.koyande@here.com>
2024-11-20 12:50:13 +0530

    Fix prodcuts endpoint

--- backend/schemas/product_schema.py
+++ backend/schemas/product_schema.py
@@ -4,3 +4,4 @@ class CreateProduct(BaseModel):
     name: str
     description: str
     hs6: str
+    user_id: int
--- backend/services/product_service.py
+++ backend/services/product_service.py
@@ -1,3 +1,4 @@
+import uuid
 from sqlalchemy.orm import Session
 from models.product_model import Product_Model
 from schemas.product_schema import CreateProduct
@@ -6,7 +7,8 @@ def add_product(product: CreateProduct, session: Session):
     new_product = Product_Model(
         product_name=product.name,
         product_description=product.description,
-        itc_hs=product.hs6
+        itc_hs=product.hs6,
+        user_id=product.user_id,
     )
     session.add(new_product)
     session.commit()
